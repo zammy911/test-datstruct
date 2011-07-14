0662863f708991584649f46326f012b59328194c
Nikolai Zamkovoi <zam@zam.org.ua>
2011-07-14 22:33:16 +0300

    ticket#1 adding fixtures

--- apps/profile/views.py
+++ apps/profile/views.py
@@ -3,7 +3,7 @@ from profile.models import *
 from django.shortcuts import get_object_or_404
 
 
-@render_to('app.html')
+@render_to('base.html')
 def show_contact(request):
     """
     Displays contacts
